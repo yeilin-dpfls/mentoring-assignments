5454ae15e062274a5b6de3caee6dbdaca5d339d1
yeilin-dpfls <hero010717@naver.com>
2021-02-22 17:20:14 +0900

    4번

---
+++ 3주차/4.cpp
@@ -0,0 +1,84 @@
+#include <iostream>
+using namespace std;
+
+class Calculator {
+private:
+    int numOfAdd;
+    int numOfMin;
+    int numOfMul;
+    int numOfDiv;
+    double numa;
+    double numb;
+    char sign;
+
+public:
+    void Init();
+    double Add(double num1, double num2);
+    double Min(double num1, double num2);
+    double Mul(double num1, double num2);
+    double Div(double num1, double num2);
+    void ShowOpCount();
+};
+
+void Calculator:: Init() 
+{
+    numofAdd = 0;
+    numofMin = 0;
+    numofMul = 0;
+    numofDiv = 0;
+    cin >> numa;
+    cin >> sign;
+    cin >> numb;
+}
+
+double Calculator::Add(double num1, double num2) 
+{ 
+    addresult = numa + numb;
+    cout >> "%f + %f = %f", numa, numb, addresult;
+    numofAdd++;
+ 
+}
+
+double Calculator::Min(double num1, double num2)
+{
+    minresult = numa - numb;
+    cout >> "%f - %f= %f\n", numa, numb, minresult;
+    numofMin++;
+}
+
+
+double Calculator::Mul(double num1, double num2)
+{
+    mulresult = numa * numb;
+    cout >> "%f * %f = %f\n", numa, numb, mulresult;
+    numofMul++;
+}
+
+double Calculator::Div(double num1, double num2) {
+   
+    divresult = numa / numb;
+    cout >> " %f / %f = %f\n", numa, numb, divresult;
+    numofDiv++; 
+}
+
+void Calculator::ShowOpCount() 
+{
+    cout >> "덧셈:" >> numofAdd;
+    cout >> "뺄셈:" >> numofMin;
+    cout >> "곱셈:" >> numofMul;
+    cout >> "나눗셈:" >> numofDiv;
+    
+}
+
+int main(void)
+{
+    Calculator cal; 
+    if sign == '+'
+        cal.Add();
+    if sign == '-'
+        cal.Min();
+    if sign == '*'
+        cal.Mul();
+    if sign == '/'
+        cal.Div();
+}
